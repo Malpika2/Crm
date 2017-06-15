0607d3d87ff2fbea0e42d5e13eae0b9d47c90b09
Malpika2 <hamthony@hotmail.com>
2017-06-15 17:09:22 -0500

    Start

---
+++ application/views/welcome_message.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Prueba Codeigniter Malpika</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>HOLA MUNDO</h1>
+</div>
+
+</body>
+</html>
